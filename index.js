1ab04b6eb87cfce736b1ed7cebeadbcbb3c1c719
TaehwanGo <gth1123@naver.com>
2020-06-19 02:46:13 +0900

    #2.5

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 // console.log("Hi!");
 
-const express = require('express')
-const app = express()
+// const express = require('express'); // 이걸 최신 자바스크립트 파일로 변경하면 
+import express from "express";
+const app = express();
 // const handleListening = () => {
 
 // }
 const PORT = 4000;
-function handleListening() {
-    console.log(`Listening on : http://localhost:${PORT}`);
+// function handleListening() {
+//     console.log(`Listening on : http://localhost:${PORT}`);
+// }
+
+const handleListening = () => console.log(`Listening on : http://localhost:${PORT}`);
+
+const handleHome = (req, res) => {
+    console.log("Hi from handleHome");
+    // console.log(req);
+    res.send("Hello from Home");
 }
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+app.get("/",handleHome);
+
+app.get("/profile", handleProfile);
 
 // respond with "hello world" when a GET request is made to the homepage
 // app.get('/', function (req, res) {
