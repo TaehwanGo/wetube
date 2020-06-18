030ea2e5c79e842ce37527458b04d9128fac24b6
TaehwanGo <gth1123@naver.com>
2020-06-18 23:18:47 +0900

    #2.3
    making a server, port : 4000
    on localhost

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hi!");
+// console.log("Hi!");
+
+const express = require('express')
+const app = express()
+// const handleListening = () => {
+
+// }
+const PORT = 4000;
+function handleListening() {
+    console.log(`Listening on : http://localhost:${PORT}`);
+}
+
+// respond with "hello world" when a GET request is made to the homepage
+// app.get('/', function (req, res) {
+//   res.send('hello world')
+// })
+
+app.listen(PORT, handleListening);
