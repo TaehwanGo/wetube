483601443df2b1f5cbfe9173c72587bcf5b92ca8
TaehwanGo <gth1123@naver.com>
2020-06-19 04:10:02 +0900

    #2.7
    Express Core : Middlewares part Two

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 // const express = require('express'); // 이걸 최신 자바스크립트 파일로 변경하면 
 import express from "express";
+import morgan from "morgan"; // 하얀글씨 morgan은 변수이름임 
+import helmet from "helmet";
+import cookeParser from "cookie-parser";
+import bodyParser from "body-parser";
 const app = express();
 // const handleListening = () => {
 
@@ -22,11 +26,19 @@ const betweenHome = (req, res, next) => {
     next(); // app.get에서 betweenHome의 다음 함수인 handleHome함수를 실행
 }
 
-app.use(betweenHome); // handleHome을 실행시키고 싶으므로 app.get("/",handleHome); 앞에 위치해야함(지금위치)
+app.use(morgan("dev")); // tiny, combined
+app.use(cookeParser());
+app.use(bodyParser.json()); // json을 수신했을때 이해
+app.use(bodyParser.urlencoded({extended: true})); // form을 수신했을때 이해
+app.use(helmet());
+// app.use(betweenHome); // handleHome을 실행시키고 싶으므로 app.get("/",handleHome); 앞에 위치해야함(지금위치)
 // 모든 경로에 betweenHome함수를 실행하는 것을 적용 
 // 여기에 모든 미들웨어를 적어주면 하나하나 실행하게 됨
+// const middleware = (req, res, next) => { // next가 아닌 이런식의 미들웨어는 연결을 끊을 수 있음 
+//     res.send("not happening");
+// };
 
-app.get("/",handleHome); // betweenHome, handleHome
+app.get("/", handleHome); // middleware, betweenHome, handleHome
 
 app.get("/profile", handleProfile);
 
