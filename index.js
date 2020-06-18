7f76ac2715a1b5ed9238b916b48f1569dcdaa594
TaehwanGo <gth1123@naver.com>
2020-06-19 03:22:38 +0900

    #2.6
    Express Core : Middlewares

--- index.js
+++ index.js
@@ -13,21 +13,21 @@ const PORT = 4000;
 
 const handleListening = () => console.log(`Listening on : http://localhost:${PORT}`);
 
-const handleHome = (req, res) => {
-    console.log("Hi from handleHome");
-    // console.log(req);
-    res.send("Hello from Home");
-}
+const handleHome = (req, res) => res.send("Hello from Home");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
-app.get("/",handleHome);
+const betweenHome = (req, res, next) => {
+    console.log("I'm between");
+    next(); // app.get에서 betweenHome의 다음 함수인 handleHome함수를 실행
+}
+
+app.use(betweenHome); // handleHome을 실행시키고 싶으므로 app.get("/",handleHome); 앞에 위치해야함(지금위치)
+// 모든 경로에 betweenHome함수를 실행하는 것을 적용 
+// 여기에 모든 미들웨어를 적어주면 하나하나 실행하게 됨
+
+app.get("/",handleHome); // betweenHome, handleHome
 
 app.get("/profile", handleProfile);
-
-// respond with "hello world" when a GET request is made to the homepage
-// app.get('/', function (req, res) {
-//   res.send('hello world')
-// })
 
 app.listen(PORT, handleListening);
