0c983d77b58ab61518f04716b07f6cfbb32ba8da
TaehwanGo <gth1123@naver.com>
2020-06-23 05:10:41 +0900

    #2.17
    Template Variables in Pug

--- middlewares.js
+++ middlewares.js
@@ -1,5 +1,6 @@
 import routes from "./routes";
 
+// local변수를 global변수화 하는 middleware
 export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = "Wetube";
     res.locals.routes = routes; // routes변수에 담은 routes.js를 전역화
