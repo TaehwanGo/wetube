68aa17547c235dd60e18287ce622f78b0fb2ef5b
TaehwanGo <gth1123@naver.com>
2020-06-23 04:56:27 +0900

    #2.16
    Local variables in Pug

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from "morgan"; // 하얀글씨 morgan은 변수이름임
 import helmet from "helmet";
 import cookeParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter"; // export default 가 아니므로 {변수명}으로 받음
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -12,15 +13,19 @@ const app = express();
 
 // const handleHome = (req, res) => res.send("Hello from Home");
 // const handleProfile = (req, res) => res.send("You are on my profile");
-
+app.use(helmet());
 app.set("view engine", "pug");
 app.use(morgan("dev")); // tiny, combined
 app.use(cookeParser());
 app.use(bodyParser.json()); // json을 수신했을때 이해
 app.use(bodyParser.urlencoded({extended: true})); // form을 수신했을때 이해
-app.use(helmet());
 // app.get("/", handleHome); // middleware, betweenHome, handleHome
 // app.get("/profile", handleProfile);
+
+// local변수를 global변수로 만드는 middleware
+
+app.use(localsMiddleware);
+
 app.use(routes.home, globalRouter);
 // app.use("/users", userRouter); // app.get에서 app.use로 바꿈 : 누군가 /user로 접속하면 이 router 전체를 사용하겠다는 의미 
 app.use(routes.users, userRouter);
