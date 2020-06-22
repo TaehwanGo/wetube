8a4900bfe507145fe10f04847405efdc4906e983
TaehwanGo <gth1123@naver.com>
2020-06-23 02:13:35 +0900

    #2.12

--- app.js
+++ app.js
@@ -13,18 +13,17 @@ const app = express();
 // const handleHome = (req, res) => res.send("Hello from Home");
 // const handleProfile = (req, res) => res.send("You are on my profile");
 
+app.set("view engine", "pug");
 app.use(morgan("dev")); // tiny, combined
 app.use(cookeParser());
 app.use(bodyParser.json()); // json을 수신했을때 이해
 app.use(bodyParser.urlencoded({extended: true})); // form을 수신했을때 이해
 app.use(helmet());
-
 // app.get("/", handleHome); // middleware, betweenHome, handleHome
 // app.get("/profile", handleProfile);
-
 app.use(routes.home, globalRouter);
 // app.use("/users", userRouter); // app.get에서 app.use로 바꿈 : 누군가 /user로 접속하면 이 router 전체를 사용하겠다는 의미 
-app.use(routes.userDetail, userRouter);
+app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
 
 export default app;
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,10 +1,21 @@
 import express from "express";
+import routes from "../routes";
+import {
+  users,
+  userDetail,
+  editProfile,
+  changePassword
+} from "../controllers/userController";
 // export default로 설정하지 않았으므로 이것을 import할때 추가로 {}로 변수를 감싸야함
 const userRouter = express.Router(); // router는 많은 route들이 담긴 파일
 
 // userRouter.get("/", (req, res) => res.send('user index'));
 // userRouter.get("/edit", (req, res) => res.send('user edit'));
 // userRouter.get("/password", (req, res) => res.send('user password'));
+userRouter.get(routes.users, users);
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter;
 
